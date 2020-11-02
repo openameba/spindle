7a4c17b09ce52c25a4f361821e5e5611677ea2f0
herablog <kz.route21@gmail.com>
2020-11-02 16:54:14 +0900

    chore(spindle-ui): Storybook addon-docs

--- packages/spindle-ui/.storybook/main.js
+++ packages/spindle-ui/.storybook/main.js
@@ -1,7 +1,8 @@
 module.exports = {
-  stories: ['../src/*.stories.ts', '../src/**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.@(ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-a11y',
+    '@storybook/addon-docs',
   ]
 };
