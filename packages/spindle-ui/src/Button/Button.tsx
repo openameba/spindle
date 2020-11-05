2d5df54f17cff8b71ef65af8271c50abe7184598
herablog <kz.route21@gmail.com>
2020-11-05 22:25:11 +0900

    feat(spindle-ui): add layout attribute to Button

--- packages/spindle-ui/src/Button/Button.tsx
+++ packages/spindle-ui/src/Button/Button.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+type Layout = 'intrinsic' | 'fillWidth';
+
 type Size = 'large' | 'medium' | 'small';
 
 type Variant = 'contained' | 'outlined' | 'neutral' | 'danger';
 
 type Props = {
+  layout?: Layout;
   size?: Size;
   variant?: Variant;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
@@ -13,13 +16,14 @@ const BLOCK_NAME = 'spui-Button';
 
 export const Button: React.FC<Props> = ({
   children,
+  layout = 'intrinsic',
   size = 'large',
   variant = 'contained',
   ...rest
 }: Props) => {
   return (
     <button
-      className={`${BLOCK_NAME} ${BLOCK_NAME}--${size} ${BLOCK_NAME}--${variant}`}
+      className={`${BLOCK_NAME} ${BLOCK_NAME}--${layout} ${BLOCK_NAME}--${size} ${BLOCK_NAME}--${variant}`}
       {...rest}
     >
       {children}
