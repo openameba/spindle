91e86af56e4daf5a82e5a5d662c00063ef7bd8e0
herablog <kz.route21@gmail.com>
2020-12-01 09:47:15 +0900

    refactor(spindle-ui): export components for project using TS < v3.8

--- packages/spindle-ui/src/index.ts
+++ packages/spindle-ui/src/index.ts
@@ -1,3 +1,6 @@
-export { Button } from './Button';
-export * as Form from './Form';
-export * as Icon from './Icon';
+import { Button } from './Button';
+import * as Form from './Form';
+import * as Icon from './Icon';
+
+// Components are currently exported after "import" for projects using TS lower v3.8 that does not support "export * as".
+export { Button, Form, Icon };
