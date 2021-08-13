5d64c038a0647e765afa13932ce8425889aa4eee
Kazunari Hara <kz.route21@gmail.com>
2021-08-13 15:10:16 +0900

    build(spindle-tokens): export datk theme, drop shadow

--- packages/spindle-tokens/.figmaexportrc.js
+++ packages/spindle-tokens/.figmaexportrc.js
@@ -11,6 +11,26 @@ module.exports = {
                     fileName: 'primitive.json'
                 })
             ]
-        }]
+        }],
+        ['styles', {
+          fileId: process.env.FIGMA_COLOR_THEME_DARK_FILE_ID,
+          outputters: [
+              exporter({
+                  output: './tokens/color',
+                  replacer: result => JSON.stringify({ Color: { Theme: result } }, null, 2),
+                  fileName: 'themedark.json' // TODO: change filename
+              })
+          ]
+      }],
+      ['styles', {
+        fileId: process.env.FIGMA_DROP_SHADOW_FILE_ID,
+        outputters: [
+            exporter({
+                output: './tokens/shadow',
+                replacer: result => JSON.stringify({ Shadow: { ['Drop Shadow']: result } }, null, 2),
+                fileName: 'drop-shadow.json'
+            })
+        ]
+    }]
     ]
 }
