51e6f87a40b42cde7282dfcb1fc813a7561a383c
keiya01 <keiya.s.0210@gmail.com>
2022-03-28 12:48:33 +0900

    feat: add storybook settings

---
+++ packages/spindle-hooks/.storybook/preview.js
@@ -0,0 +1,23 @@
+export const parameters = {
+  backgrounds: {
+    default: 'Surface Primary',
+    values: [
+      {
+        name: 'Background Color',
+        value: '#F5F6F6',
+      },
+      {
+        name: 'Surface Primary',
+        value: '#FFFFFF',
+      },
+      {
+        name: 'Surface Secondary',
+        value: 'rgba(8,18,26,0.04)',
+      },
+      {
+        name: 'Background Color Dark',
+        value: '#08121A'
+      }
+    ],
+  },
+};
