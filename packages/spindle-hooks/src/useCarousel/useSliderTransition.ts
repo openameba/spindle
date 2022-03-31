d62bd353bcf02a38a77a91e7374ce12ba672773c
keiya01 <keiya.s.0210@gmail.com>
2022-03-31 15:01:12 +0900

    feat(spindle-hooks): add useCarousel hooks

---
+++ packages/spindle-hooks/src/useCarousel/useSliderTransition.ts
@@ -0,0 +1,57 @@
+import { useMemo, useState, MutableRefObject } from 'react';
+import { useValueRef } from './useValueRef';
+
+type Payload = {
+  copyCount: number;
+  itemCount: number;
+  linkRefs: MutableRefObject<HTMLLinkElement[]>;
+  isAutoPlaying: boolean;
+  isFocus: boolean;
+};
+
+export function useSliderTransition({
+  copyCount,
+  itemCount,
+  linkRefs,
+  isAutoPlaying,
+  isFocus,
+}: Payload) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentIndexRef = useValueRef(currentIndex);
+  const [disableTransition, setDisableTransition] = useState(false);
+  const [disableAutoFocus, setDisableAutoFocus] = useState(false);
+
+  const listStyles = useMemo(() => {
+    const transitionStyle = disableTransition ? { transition: 'none' } : {};
+    return {
+      ...transitionStyle,
+      transform: `translate3d(${
+        -100 * (currentIndex + copyCount)
+      }%, 0, 0) translateX(0)`,
+    };
+  }, [copyCount, currentIndex, disableTransition]);
+
+  const handleTransitionEnd = () => {
+    // if reach contents end, rewind without transition to make it look like looping
+    if (!isFocus) {
+      setDisableTransition(true);
+      setCurrentIndex((prev) => (prev + itemCount) % itemCount);
+      if (!isAutoPlaying && !disableAutoFocus) {
+        linkRefs.current[
+          ((currentIndex + itemCount) % itemCount) + copyCount
+        ].focus();
+      }
+    }
+  };
+
+  return {
+    currentIndexRef,
+    currentIndex,
+    handleTransitionEnd,
+    listStyles,
+    setCurrentIndex,
+    setDisableAutoFocus,
+    disableTransition,
+    setDisableTransition,
+  };
+}
