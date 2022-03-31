10f6dd91e5e761d4d48b49539651ac3b6c4c029a
keiya01 <keiya.s.0210@gmail.com>
2022-03-31 15:00:54 +0900

    feat(spindle-hooks): add spindle-hooks pkg

---
+++ packages/spindle-hooks/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../src/**/*.stories.@(ts|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-docs',
+    '@storybook/addon-viewport',
+  ]
+};
