a14b93d969169527269b31424e59e9f36468df6c
Kazunari Hara <kz.route21@gmail.com>
2022-05-26 17:33:19 +0900

    feat(spindle-ui): create TextLink

---
+++ packages/spindle-ui/src/TextLink/TextLink.tsx
@@ -0,0 +1,42 @@
+import React, { forwardRef } from 'react';
+
+type Variant = 'subtle';
+
+interface Props
+  extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'className'> {
+  children?: React.ReactNode;
+  variant?: Variant;
+  icon?: React.ReactNode;
+  iconPosition?: 'start' | 'end';
+}
+
+const BLOCK_NAME = 'spui-TextLink';
+
+export const TextLink = forwardRef<HTMLAnchorElement, Props>(function TextLink(
+  { children, variant, icon, iconPosition, ...rest }: Props,
+  ref,
+) {
+  return (
+    <a
+      className={[
+        BLOCK_NAME,
+        variant && `${BLOCK_NAME}--${variant}`,
+        icon && `${BLOCK_NAME}--hasIcon`,
+        icon && iconPosition && `${BLOCK_NAME}--icon${iconPosition}`,
+      ]
+        .filter(Boolean)
+        .join(' ')}
+      ref={ref}
+      {...rest}
+    >
+      {icon ? (
+        <>
+          <span className={`${BLOCK_NAME}-icon`}>{icon}</span>
+          {children}
+        </>
+      ) : (
+        children
+      )}
+    </a>
+  );
+});
