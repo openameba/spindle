de56b8389719ae47ff5a532d4a1b6bce572e2964
Kazunari Hara <kz.route21@gmail.com>
2022-09-06 11:37:22 +0900

    feat(spindle-ui): scrollable breadcrumb

--- packages/spindle-ui/src/Breadcrumb/BreadcrumbList.tsx
+++ packages/spindle-ui/src/Breadcrumb/BreadcrumbList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import ChevronRightBold from '../Icon/ChevronRightBold';
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
@@ -9,6 +9,12 @@ const BLOCK_NAME = 'spui-Breadcrumb';
 
 export const BreadcrumbList = (props: Props) => {
   const { children, className, ...rest } = props;
+  const currentRef = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    currentRef?.current?.scrollIntoView();
+  }, []);
+
   return (
     <nav
       aria-label="パンくずリスト"
@@ -18,7 +24,12 @@ export const BreadcrumbList = (props: Props) => {
       <ol className={`${BLOCK_NAME}-list`}>
         {React.Children.map(children, (child) => {
           return React.isValidElement(child) ? (
-            <li className={`${BLOCK_NAME}-item`}>
+            <li
+              className={`${BLOCK_NAME}-item`}
+              {...(child.props.current || child.props['aria-current']
+                ? { ref: currentRef }
+                : {})}
+            >
               {child}
               <ChevronRightBold
                 aria-hidden="true"
