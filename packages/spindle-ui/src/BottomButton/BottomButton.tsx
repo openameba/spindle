5becb9747c32511f4fbf2cfe63d51117069673ed
nozaki_tatsuya <nozaki_tatsuya@cyberagent.co.jp>
2022-09-30 18:10:11 +0900

    fix(spindle-ui): do not accept div tag attributes

--- packages/spindle-ui/src/BottomButton/BottomButton.tsx
+++ packages/spindle-ui/src/BottomButton/BottomButton.tsx
@@ -3,24 +3,24 @@ import React, { ReactNode } from 'react';
 type Position = 'fixed' | 'sticky';
 
 type Props = {
-  position?: Position;
   children: ReactNode;
-} & React.HTMLAttributes<HTMLDivElement>;
+  className?: string;
+  position?: Position;
+};
 
 const BLOCK_NAME = 'spui-BottomButton';
 
 export const BottomButton: React.FC<Props> = ({
   children,
-  className,
+  className = '',
   position = 'fixed',
-  ...rest
 }: Props) => {
   const classnames = [BLOCK_NAME, `${BLOCK_NAME}--${position}`, className]
     .filter(Boolean)
     .join(' ');
 
   return (
-    <div className={classnames} {...rest}>
+    <div className={classnames}>
       <div className={`${BLOCK_NAME}-wrap`}>{children}</div>
     </div>
   );
