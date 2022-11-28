7ccab3fdf907f1d3d0eb56813aaeb96bba789197
yasuda_shin <yasuda_shin@cyberagent.co.jp>
2022-11-28 15:38:05 +0900

    fix(spindle-ui): pagination test case

--- packages/spindle-ui/src/Pagination/hooks/useShowItem.test.ts
+++ packages/spindle-ui/src/Pagination/hooks/useShowItem.test.ts
@@ -1,44 +1,601 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { useShowItem } from './useShowItem';
 
+// ページ総数の閾値
+const TOTAL_THRESHOLD = 100;
+// 表示出来る数字(アイテム)の最大数
+const MAX_SHOW_ITEM_SIZE = 5;
+
 describe('useShowItem()', () => {
-  it('should return show props', () => {
-    const current = 8;
-    const total = 20;
-
-    const { result } = renderHook(() => useShowItem({ current, total }));
-
-    expect(result.current.displayItem).toEqual([1, 7, 8, 9, 20]);
-    expect(result.current.hideDisplayItem).toEqual(true);
-  });
-
-  it('should return show props is current first', () => {
+  it('use show item current 1 total 1', () => {
     const current = 1;
-    const total = 20;
+    const total = 1;
 
-    const { result } = renderHook(() => useShowItem({ current, total }));
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
 
-    expect(result.current.displayItem).toEqual([1, 2, 3, 4, 20]);
-    expect(result.current.hideDisplayItem).toEqual(true);
+    expect(result.current).toEqual([1]);
   });
 
-  it('should return show props is current last', () => {
-    const current = 20;
-    const total = 20;
+  it('use show item current 1 total 2', () => {
+    const current = 1;
+    const total = 2;
 
-    const { result } = renderHook(() => useShowItem({ current, total }));
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
 
-    expect(result.current.displayItem).toEqual([1, 17, 18, 19, 20]);
-    expect(result.current.hideDisplayItem).toEqual(true);
+    expect(result.current).toEqual([1, 2]);
   });
 
-  it('should return show props is total is less than max page item', () => {
+  it('use show item current 2 total 2', () => {
+    const current = 2;
+    const total = 2;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2]);
+  });
+
+  it('use show item current 1 total 3', () => {
+    const current = 1;
+    const total = 3;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3]);
+  });
+
+  it('use show item current 2 total 3', () => {
     const current = 2;
     const total = 3;
 
-    const { result } = renderHook(() => useShowItem({ current, total }));
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
 
-    expect(result.current.displayItem).toEqual([1, 2, 3]);
-    expect(result.current.hideDisplayItem).toEqual(false);
+    expect(result.current).toEqual([1, 2, 3]);
+  });
+
+  it('use show item current 3 total 3', () => {
+    const current = 3;
+    const total = 3;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3]);
+  });
+
+  it('use show item current 1 total 4', () => {
+    const current = 1;
+    const total = 4;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4]);
+  });
+
+  it('use show item current 2 total 4', () => {
+    const current = 2;
+    const total = 4;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4]);
+  });
+
+  it('use show item current 3 total 4', () => {
+    const current = 3;
+    const total = 4;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4]);
+  });
+
+  it('use show item current 4 total 4', () => {
+    const current = 4;
+    const total = 4;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4]);
+  });
+
+  it('use show item current 1 total 5', () => {
+    const current = 1;
+    const total = 5;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('use show item current 2 total 5', () => {
+    const current = 2;
+    const total = 5;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('use show item current 3 total 5', () => {
+    const current = 3;
+    const total = 5;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('use show item current 4 total 5', () => {
+    const current = 4;
+    const total = 5;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('use show item current 5 total 5', () => {
+    const current = 5;
+    const total = 5;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('use show item current 1 total 6', () => {
+    const current = 1;
+    const total = 6;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 6]);
+  });
+
+  it('use show item current 2 total 6', () => {
+    const current = 2;
+    const total = 6;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 6]);
+  });
+
+  it('use show item current 3 total 6', () => {
+    const current = 3;
+    const total = 6;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 3, 4, 5, 6]);
+  });
+
+  it('use show item current 4 total 6', () => {
+    const current = 4;
+    const total = 6;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 3, 4, 5, 6]);
+  });
+
+  it('use show item current 5 total 6', () => {
+    const current = 5;
+    const total = 6;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 3, 4, 5, 6]);
+  });
+
+  it('use show item current 6 total 6', () => {
+    const current = 6;
+    const total = 6;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 3, 4, 5, 6]);
+  });
+
+  it('use show item current 1 total 7', () => {
+    const current = 1;
+    const total = 7;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 7]);
+  });
+
+  it('use show item current 2 total 7', () => {
+    const current = 2;
+    const total = 7;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 7]);
+  });
+
+  it('use show item current 3 total 7', () => {
+    const current = 3;
+    const total = 7;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 3, 4, 5, 7]);
+  });
+
+  it('use show item current 4 total 7', () => {
+    const current = 4;
+    const total = 7;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 4, 5, 6, 7]);
+  });
+
+  it('use show item current 5 total 7', () => {
+    const current = 5;
+    const total = 7;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 4, 5, 6, 7]);
+  });
+
+  it('use show item current 6 total 7', () => {
+    const current = 6;
+    const total = 7;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 4, 5, 6, 7]);
+  });
+
+  it('use show item current 7 total 7', () => {
+    const current = 7;
+    const total = 7;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 4, 5, 6, 7]);
+  });
+
+  it('use show item current 1 total 1000', () => {
+    const current = 1;
+    const total = 1000;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('use show item current 2 total 1000', () => {
+    const current = 2;
+    const total = 1000;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('use show item current 3 total 1000', () => {
+    const current = 3;
+    const total = 1000;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('use show item current 4 total 1000', () => {
+    const current = 4;
+    const total = 1000;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([2, 3, 4, 5, 6]);
+  });
+
+  it('use show item current 5 total 1000', () => {
+    const current = 5;
+    const total = 1000;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([3, 4, 5, 6, 7]);
+  });
+
+  it('use show item current 200 total 1000', () => {
+    const current = 200;
+    const total = 1000;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([198, 199, 200, 201, 202]);
+  });
+
+  it('use show item current 998 total 1000', () => {
+    const current = 998;
+    const total = 1000;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([996, 997, 998, 999, 1000]);
+  });
+
+  it('use show item current 999 total 1000', () => {
+    const current = 999;
+    const total = 1000;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([996, 997, 998, 999, 1000]);
+  });
+
+  it('use show item current 1000 total 1000', () => {
+    const current = 1000;
+    const total = 1000;
+
+    const { result } = renderHook(() =>
+      useShowItem({
+        current,
+        total,
+        totalThreshold: TOTAL_THRESHOLD,
+        maxShowItemSize: MAX_SHOW_ITEM_SIZE,
+      }),
+    );
+
+    expect(result.current).toEqual([996, 997, 998, 999, 1000]);
   });
 });
