ef69a0aa02417e0cbcb642b526f419caacd818f8
yasuda_shin <yasuda_shin@cyberagent.co.jp>
2022-12-05 14:33:09 +0900

    refactor(spindle-ui): use show item comment

--- packages/spindle-ui/src/Pagination/hooks/useShowItem.ts
+++ packages/spindle-ui/src/Pagination/hooks/useShowItem.ts
@@ -8,19 +8,31 @@ type Payload = {
 // 表示出来る数字（アイテム）の最大数
 const MAX_SHOW_ITEM_SIZE = 5;
 
+/**
+ * useShowItem分岐条件
+ * 1.総ページ数が表示したいアイテム数以下の場合、総ページ数と同じアイテム数を返す
+ * 2.総ページ数がアイテムの表示最大数以下、または総ページ数が閾値以上の場合
+ *   1.現在のページ数が中央値 + 1より小さい場合、表示したいアイテム数を最初のページから返す
+ *   2.現在のページ数が総ページ数 - 中央値より大きい場合、総ページ数からさかのぼった表示したいアイテム数を返す
+ *   3.現在のページ数と前後に表示したいアイテム数を返す
+ * 3.上記に該当しない場合は以下の処理を行う
+ *   1.現在のページ数が中央値 + 1より小さい場合、表示したいアイテム数を最初のページから返し最後は総ページ数を返す
+ *   2.現在のページ数が総ページ数 - 中央値より大きい場合、最初のページとtotalまでの表示したいアイテム数を返す
+ *   3.最初のアイテムは1を最後のアイテムは総ページ数を返すとともにlengthが3の場合は現在のページ数のみでlengthが5の場合は現在のページ数とその前後のアイテム数を返す
+ */
 export function useShowItem({
   current,
   total,
   showItemSize,
   totalThreshold,
 }: Payload) {
-  // 総数が表示したいアイテム数に満たない場合
   if (total <= showItemSize) {
     return Array.from({ length: total }, (_e, index) => index + 1);
   }
+
   // 中央値（少数の切り捨て）
   const median = (showItemSize / 2) | 0;
-  // 総数がアイテムの表示最大数に満たない、または総数が閾値を超える場合
+
   if (total <= MAX_SHOW_ITEM_SIZE || total >= totalThreshold) {
     if (current < 1 + median) {
       return Array.from({ length: showItemSize }, (_e, index) => index + 1);
@@ -36,14 +48,12 @@ export function useShowItem({
       (_e, index) => current - median + index,
     );
   }
-  // 総数がアイテムの表示最大数以上の場合
   if (current < 1 + median) {
     return [
       ...Array.from({ length: showItemSize - 1 }, (_e, index) => index + 1),
       total,
     ];
   }
-  // 総数がアイテムの表示最大数以下の場合
   if (current > total - median) {
     return [
       1,
