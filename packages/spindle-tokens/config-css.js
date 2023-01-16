846b7fe6bb9794621ca5c58f017ee9f3d66282b2
hara-kazunari <kz.route21@gmail.com>
2023-01-16 18:19:33 +0900

    feat(spindle-tokens): create animation token for css

---
+++ packages/spindle-tokens/config-css.js
@@ -0,0 +1,46 @@
+function makeAnimationTokens(dictionary) {
+  return dictionary.allTokens
+    .filter((token) => {
+      return token.attributes.category === 'Animation';
+    })
+    .map((token) => {
+      if (token.attributes.type === 'Easing') {
+        token.value = `cubic-bezier(${token.value})`;
+        return token;
+      }
+
+      if (dictionary.usesReference(token.original.value)) {
+        const [ref] = dictionary.getReferences(token.original.value);
+        token.value = `var(--${ref.name})`;
+        return token;
+      }
+
+      return token;
+    })
+    .map((token) => `  --${token.name}: ${token.value};`);
+}
+
+module.exports = {
+  source: ['tokens/**/*.json'],
+  format: {
+    cssAnimation: ({ dictionary }) => {
+      const tokens = makeAnimationTokens(dictionary);
+      return [':root {', ...tokens, '}', ''].join('\n');
+    },
+  },
+  platforms: {
+    css: {
+      transforms: ['attribute/cti', 'name/cti/kebab'],
+      files: [
+        {
+          destination: 'dist/css/spindle-tokens-animation.css',
+          format: 'cssAnimation',
+        },
+        {
+          destination: 'dist/css/spindle-tokens.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
+};
