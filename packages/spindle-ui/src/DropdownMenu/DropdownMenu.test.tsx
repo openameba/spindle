975e81dba3a00bdc7dae7efb0024745624dfb8e7
chiba_kento <chiba_kento@cyberagent.co.jp>
2023-12-07 19:34:06 +0900

    fix(spindle-ui): esc handler in DropdownMenu
    The escape handler should be attached only when the drop menu is opened.

--- packages/spindle-ui/src/DropdownMenu/DropdownMenu.test.tsx
+++ packages/spindle-ui/src/DropdownMenu/DropdownMenu.test.tsx
@@ -8,8 +8,8 @@ import { Button } from '../Button';
 import { BLOCK_NAME, DropdownMenu } from './DropdownMenu';
 
 const ANIMATION_DURATION = 300;
-const useDropdownMenuOpen = () => {
-  const [open, setOpen] = useState(true);
+const useDropdownMenuOpen = (initialState?: boolean) => {
+  const [open, setOpen] = useState(initialState ?? true);
   const onClick = () => {
     setOpen((prevOpen) => !prevOpen);
   };
@@ -226,5 +226,138 @@ describe('<DropdownMenu />', () => {
     expect(
       screen.getByText(/triggerButton/).nextElementSibling,
     ).not.toBeInTheDocument();
+  });
+
+  test('The DropdownMenu should be closed when you trigger esc key.', async () => {
+    const triggerRef = createRef<HTMLButtonElement>();
+    const onMenuButtonClick = jest.fn();
+    const { result } = renderHook(() => useDropdownMenuOpen());
+    const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
+
+    expect(result.current.open).toBe(true);
+
+    const { rerender } = render(
+      <>
+        <DropdownMenu.Frame>
+          <Button onClick={result.current.onClick} ref={triggerRef}>
+            triggerButton
+          </Button>
+          <DropdownMenu.List
+            onClose={result.current.onClose}
+            open={result.current.open}
+            triggerRef={triggerRef}
+          >
+            <DropdownMenu.ListItem onClick={onMenuButtonClick}>
+              <DropdownMenu.Title>testTitle</DropdownMenu.Title>
+            </DropdownMenu.ListItem>
+          </DropdownMenu.List>
+        </DropdownMenu.Frame>
+      </>,
+    );
+
+    const menu = screen.getByText(/triggerButton/).nextElementSibling;
+    expect(menu).toHaveClass(`${BLOCK_NAME}-menu`);
+    expect(menu).toBeInTheDocument();
+
+    await user.keyboard('{esc}');
+
+    expect(menu).toHaveClass('is-fade-out');
+
+    act(() => {
+      jest.advanceTimersByTime(ANIMATION_DURATION);
+      if (menu) fireEvent.animationEnd(menu);
+    });
+
+    rerender(
+      <>
+        <DropdownMenu.Frame>
+          <Button onClick={result.current.onClick} ref={triggerRef}>
+            triggerButton
+          </Button>
+          <DropdownMenu.List
+            onClose={result.current.onClose}
+            open={result.current.open}
+            triggerRef={triggerRef}
+          >
+            <DropdownMenu.ListItem onClick={onMenuButtonClick}>
+              <DropdownMenu.Title>testTitle</DropdownMenu.Title>
+            </DropdownMenu.ListItem>
+          </DropdownMenu.List>
+        </DropdownMenu.Frame>
+      </>,
+    );
+
+    expect(result.current.open).toBe(false);
+    expect(
+      screen.getByText(/triggerButton/).nextElementSibling,
+    ).not.toBeInTheDocument();
+  });
+
+  test('The active element should be trigger element after the escape keydown when the Dropdown menu is opened.', async () => {
+    const triggerRef = createRef<HTMLButtonElement>();
+    const onMenuButtonClick = jest.fn();
+    const { result } = renderHook(() => useDropdownMenuOpen());
+    const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
+
+    expect(result.current.open).toBe(true);
+
+    render(
+      <>
+        <DropdownMenu.Frame>
+          <Button onClick={result.current.onClick} ref={triggerRef}>
+            triggerButton
+          </Button>
+          <DropdownMenu.List
+            onClose={result.current.onClose}
+            open={result.current.open}
+            triggerRef={triggerRef}
+          >
+            <DropdownMenu.ListItem onClick={onMenuButtonClick}>
+              <DropdownMenu.Title>testTitle</DropdownMenu.Title>
+            </DropdownMenu.ListItem>
+          </DropdownMenu.List>
+        </DropdownMenu.Frame>
+      </>,
+    );
+
+    await user.keyboard('{esc}');
+    act(() => {
+      jest.advanceTimersByTime(ANIMATION_DURATION);
+    });
+    expect(document.activeElement).toBe(triggerRef.current);
+  });
+
+  test('The active element should not be trigger element after the escape keydown when the Dropdown menu is closed.', async () => {
+    const triggerRef = createRef<HTMLButtonElement>();
+    const onMenuButtonClick = jest.fn();
+    const { result } = renderHook(() => useDropdownMenuOpen(false));
+    const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
+
+    expect(result.current.open).toBe(false);
+
+    render(
+      <>
+        <DropdownMenu.Frame>
+          <Button onClick={result.current.onClick} ref={triggerRef}>
+            triggerButton
+          </Button>
+          <DropdownMenu.List
+            onClose={result.current.onClose}
+            open={result.current.open}
+            triggerRef={triggerRef}
+          >
+            <DropdownMenu.ListItem onClick={onMenuButtonClick}>
+              <DropdownMenu.Title>testTitle</DropdownMenu.Title>
+            </DropdownMenu.ListItem>
+          </DropdownMenu.List>
+        </DropdownMenu.Frame>
+      </>,
+    );
+
+    await user.keyboard('{esc}');
+    act(() => {
+      jest.advanceTimersByTime(ANIMATION_DURATION);
+    });
+    expect(document.activeElement).not.toBe(triggerRef.current);
   });
 });
