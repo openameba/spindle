2ee3ebabaa67f2e4931fb4ae50687d667afa8257
dazy1030 <odajima_naoki@cyberagent.co.jp>
2024-06-17 23:37:55 +0900

    feat(ios): init package

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Spindle",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "Spindle",
+            targets: [
+            ]
+        ),
+    ],
+    targets: [
+    ]
+)
