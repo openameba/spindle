85ecdf5e9f59d54e49aad2c143dfc970482193d2
Kazunari Hara <kz.route21@gmail.com>
2024-09-10 22:39:27 +0900

    chore(spindle-tokens): update config for v4

--- packages/spindle-tokens/config.js
+++ packages/spindle-tokens/config.js
@@ -1,27 +1,34 @@
-module.exports = {
-  source: [`tokens/**/!(*.css).json`],
-  format: {
-    jsonDataSource: ({ dictionary }) => {
-      // Flatten data to be a data source of some querying use cases
-      // Stringify the child values to ensure the types are the same, but original.value keeps their type
-      const tokens = dictionary.allTokens.map((token) => ({
-        ...token,
-        value: `${token.value}`, // ensure the value type is string
-        pathString: token.path.join('.'), // useful for filtering the token
-      }));
-      return JSON.stringify(tokens, null, 2);
-    },
+import StyleDictionary from 'style-dictionary';
+
+const transitionOnlyFilter = {
+  name: 'transition-only',
+  filter: async (token) => {
+    return token.$type === 'transition' || token.$type === 'cubicBezier';
   },
+};
+
+StyleDictionary.registerFilter(transitionOnlyFilter);
+
+export default {
+  source: [`tokens/**/*.json`],
   platforms: {
-    json: {
+    css: {
+      transformGroup: 'css',
       files: [
         {
-          destination: 'dist/json/spindle-tokens.json',
-          format: 'json',
+          destination: 'dist/spindle-tokens.css',
+          format: 'css/variables',
+          options: {
+            outputReferences: true,
+          },
         },
         {
-          destination: 'dist/json/spindle-tokens-flat.json',
-          format: 'jsonDataSource',
+          destination: 'dist/animation.css',
+          format: 'css/variables',
+          options: {
+            outputReferences: true,
+          },
+          filter: 'transition-only',
         },
       ],
     },
