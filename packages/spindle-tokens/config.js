9e1d990b7a0ae17cd05d09e23261d34ed28e5fe5
Kazunari Hara <kz.route21@gmail.com>
2024-09-11 18:52:13 +0900

    chore(spindle-tokens): update config

--- packages/spindle-tokens/config.js
+++ packages/spindle-tokens/config.js
@@ -1,13 +1,38 @@
-const transitionOnlyFilter = {
-  name: 'transition-only',
+const transitionFilter = {
+  name: 'transition',
   filter: async (token) => {
     return token.$type === 'transition' || token.$type === 'cubicBezier';
   },
 };
 
+const primitiveColorFilter = {
+  name: 'primitive-color',
+  filter: async (token) => {
+    return token.filePath.endsWith('primitive-color.json');
+  },
+};
+
+const themeLightFilter = {
+  name: 'theme-light',
+  filter: async (token) => {
+    return token.filePath.endsWith('theme-light.json');
+  },
+};
+
+const themeDarkFilter = {
+  name: 'theme-dark',
+  filter: async (token) => {
+    return token.filePath.endsWith('theme-dark.json');
+  },
+};
+
 // Using dynamic import until ESM is supported in this repogitory
 import('style-dictionary').then((module) => {
-  module.default.registerFilter(transitionOnlyFilter);
+  const StyleDictionary = module.default;
+  StyleDictionary.registerFilter(transitionFilter);
+  StyleDictionary.registerFilter(primitiveColorFilter);
+  StyleDictionary.registerFilter(themeLightFilter);
+  StyleDictionary.registerFilter(themeDarkFilter);
 });
 
 module.exports = {
@@ -17,19 +42,36 @@ module.exports = {
       transformGroup: 'css',
       files: [
         {
-          destination: 'dist/spindle-tokens.css',
+          destination: 'dist/css/spindle-tokens-primitive-color.css',
           format: 'css/variables',
           options: {
             outputReferences: true,
           },
+          filter: 'primitive-color',
         },
         {
-          destination: 'dist/animation.css',
+          destination: 'dist/css/spindle-tokens-theme-light.css',
           format: 'css/variables',
           options: {
             outputReferences: true,
           },
-          filter: 'transition-only',
+          filter: 'theme-light',
+        },
+        {
+          destination: 'dist/css/spindle-tokens-animation.css',
+          format: 'css/variables',
+          options: {
+            outputReferences: true,
+          },
+          filter: 'transition',
+        },
+        {
+          destination: 'dist/css/spindle-tokens.css',
+          format: 'css/variables',
+          options: {
+            outputReferences: true,
+          },
+          filter: 'transition',
         },
       ],
     },
