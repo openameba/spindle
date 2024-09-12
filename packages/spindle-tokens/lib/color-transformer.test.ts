b618a747a3b70fb099dfe51b52cf7ae1defa626f
Kazunari Hara <kz.route21@gmail.com>
2024-09-12 15:55:13 +0900

    test(spindle-tokens): add transformer test

---
+++ packages/spindle-tokens/lib/color-transformer.test.ts
@@ -0,0 +1,57 @@
+import {
+  transformPrimitiveColor,
+  transformThemeColor,
+} from './color-transformer';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colorResponse = require('./color-response.json');
+
+describe('transform color', () => {
+  it('should transform primitive color correctly', () => {
+    const variables = colorResponse;
+    const transformedProperties = transformPrimitiveColor(variables);
+
+    expect(transformedProperties).toEqual({
+      'Brand Color/Ameba Green': { $type: 'color', $value: '#2d8c3c' },
+      'Brand Color/Ameba Black': { $type: 'color', $value: '#000000' },
+    });
+  });
+
+  it('should transform theme color correctly', () => {
+    const variables = colorResponse;
+    const transformedProperties = transformThemeColor(variables);
+
+    expect(transformedProperties).toEqual({
+      Light: {
+        'Background/Base': {
+          $type: 'color',
+          $value: '{Primitive Color.Gray/Gray 5}',
+        },
+        'Surface/Primary': {
+          $type: 'color',
+          $value: '{Primitive Color.White/White 100}',
+        },
+      },
+      Dark: {
+        'Background/Base': {
+          $type: 'color',
+          $value: '{Primitive Color.Gray/Gray 100}',
+        },
+        'Surface/Primary': {
+          $type: 'color',
+          $value: '{Primitive Color.Gray/Gray 90}',
+        },
+      },
+      Green: {
+        'Background/Base': {
+          $type: 'color',
+          $value: '{Primitive Color.Primary Green/Primary Green 90}',
+        },
+        'Surface/Primary': {
+          $type: 'color',
+          $value: '{Primitive Color.Primary Green/Primary Green 70}',
+        },
+      },
+    });
+  });
+});
