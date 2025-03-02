e46e030fbe692d436ef0a4fe9dc464aea2073a28
yanagi-kota <yanagi_kota@cyberagent.co.jp>
2025-03-02 16:52:30 +0900

    feat(spindle-ui): xmlnsとxmlns:linkを出力結果から除外するようにした。

--- packages/spindle-ui/src/Icon/DragIndicator.tsx
+++ packages/spindle-ui/src/Icon/DragIndicator.tsx
@@ -2,15 +2,14 @@ import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgDragIndicator = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
-    fill="currentColor"
     viewBox="0 0 24 24"
+    fill="currentColor"
     role="img"
     {...props}
   >
-    <path d="M10.5 6a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m6 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-6 6a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m6 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-6 6a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m6 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+    <path d="M10.5 6a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M16.5 6a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M10.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M16.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M10.5 18a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M16.5 18a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
   </svg>
 );
 export default SvgDragIndicator;
