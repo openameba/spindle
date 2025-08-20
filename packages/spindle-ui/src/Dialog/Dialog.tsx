5564d0e7b24729a70a545211a82008bd6445a69f
pagu <yanagikouta0602@gmail.com>
2025-08-20 14:47:40 +0900

    feat(spindle-ui): react-merge-refsをuse-callback-refに置き換え (#1351)

--- packages/spindle-ui/src/Dialog/Dialog.tsx
+++ packages/spindle-ui/src/Dialog/Dialog.tsx
@@ -5,19 +5,13 @@ import React, {
   useRef,
   useState,
 } from 'react';
-import mergeRefs from 'react-merge-refs';
+import { useMergeRefs } from 'use-callback-ref';
 import { ButtonGroup as Group } from '../ButtonGroup';
 
 interface DialogProps extends React.DialogHTMLAttributes<HTMLElement> {
   children?: React.ReactNode;
   onCancel?: (event: React.BaseSyntheticEvent) => void;
   onClose?: (event: React.BaseSyntheticEvent) => void;
-}
-
-export interface DialogHTMLElement extends HTMLElement {
-  close?: () => void;
-  showModal?: (returnValue?: string) => void;
-  open?: boolean;
 }
 
 interface PartsProps {
@@ -29,19 +23,19 @@ interface PartsProps {
 const BLOCK_NAME = 'spui-Dialog';
 const FADE_OUT_ANIMATION = 'spui-Dialog-fade-out';
 
-const Frame = forwardRef<DialogHTMLElement, DialogProps>(function Dialog(
+const Frame = forwardRef<HTMLDialogElement, DialogProps>(function Dialog(
   { children, className, open, onClose, ...rest },
   ref,
 ) {
   const [closing, setClosing] = useState(false);
-  const dialogEl = useRef<DialogHTMLElement>(null);
+  const dialogEl = useRef<HTMLDialogElement>(null);
 
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // To be closed with the open prop
     typeof onClose === 'function' && onClose(event);
   };
 
-  const handleDialogClick = (event: React.MouseEvent<DialogHTMLElement>) => {
+  const handleDialogClick = (event: React.MouseEvent<HTMLDialogElement>) => {
     // Detect backdrop click
     if (event.target === dialogEl.current) {
       onClose && onClose(event);
@@ -49,7 +43,7 @@ const Frame = forwardRef<DialogHTMLElement, DialogProps>(function Dialog(
   };
 
   const handleDialogClose = (
-    event: React.SyntheticEvent<DialogHTMLElement>,
+    event: React.SyntheticEvent<HTMLDialogElement>,
   ) => {
     // Detect escape key type
     if (event.target === dialogEl.current) {
@@ -103,7 +97,7 @@ const Frame = forwardRef<DialogHTMLElement, DialogProps>(function Dialog(
 
   return (
     <dialog
-      ref={mergeRefs([dialogEl, ref])}
+      ref={useMergeRefs([dialogEl, ref])}
       className={[BLOCK_NAME, closing && `${BLOCK_NAME}--closing`, className]
         .filter(Boolean)
         .join(' ')
