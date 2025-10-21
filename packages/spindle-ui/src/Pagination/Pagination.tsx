433d2c32de4322a8db514866d581bc6dc7f5ac83
yasuda_shin <yasuda_shin@cyberagent.co.jp>
2025-10-21 13:29:15 +0900

    fix: delete pagination aria label

--- packages/spindle-ui/src/Pagination/Pagination.tsx
+++ packages/spindle-ui/src/Pagination/Pagination.tsx
@@ -187,11 +187,7 @@ export const Pagination = (props: Props) => {
         )}
       </ul>
       {showTotal && (
-        /* biome-ignore lint/a11y/useAriaPropsSupportedByRole: Temporarily allow aria-label on paragraph element */
-        <p
-          className={`${BLOCK_NAME}-total`}
-          aria-label={`${total}ページ中の${current}ページ目`}
-        >
+        <p className={`${BLOCK_NAME}-total`}>
           {current}/{total}ページ
         </p>
       )}
