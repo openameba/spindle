7e961317e7d209f0c50772a449efea944c7b3c43
Sora_339 <xzhouzhi@gmail.com>
2026-03-02 16:59:56 +0900

    fix: 余計なプロパティを削除

--- packages/spindle-ui/src/Form/DropDown/DropDown.tsx
+++ packages/spindle-ui/src/Form/DropDown/DropDown.tsx
@@ -55,7 +55,6 @@ export const DropDown = forwardRef<HTMLSelectElement, Props>(function DropDown(
       className={[
         `${BLOCK_NAME}-label`,
         isActive ? 'is-active' : '',
-        selectDisabled ? 'is-disabled' : '',
         hasError ? 'is-error' : '',
       ]
         .filter(Boolean)
